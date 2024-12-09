b8a48d749cdeb0c67f811f39e32e7dd1442d58c4
BartSterrenburg <sb.sterrenburg@student.avans.nl>
2024-12-09 22:40:30 +0100

    changed-variabels

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,10 +3,10 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
-    SERVER_API_URL: 'https://nxworkshop.azurewebsites.net/api',
+    ROOT_DOMAIN_URL: 'https://happy-pebble-06ed2bf03.5.azurestaticapps.net',
+    SERVER_API_URL: 'https://train-api.azurewebsites.net',
 
 
     // this is permitted for now
-    MONGO_DB_CONNECTION_STRING: 'mongodb://remote-host/mongodb'
+    MONGO_DB_CONNECTION_STRING: 'mongodb+srv://sbsterrenburg:PzYmiOkbw5bjwq89@traindb.c6xon.mongodb.net/'
 };
